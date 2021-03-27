6dc8d12100ad5d27e30cd3cb714a034a9b38346a
niladmiran <niladmiran@gmail.com>
2021-03-28 00:14:12 +0800

    add the getRow function of Pascal

--- src/pascalTriangle.cpp
+++ src/pascalTriangle.cpp
@@ -24,3 +24,25 @@ std::vector<std::vector<int> > generate(int numRows) {
     }
     return result;
 }
+
+// https://leetcode.com/explore/learn/card/recursion-i/251/scenario-i-recurrence-relation/3234/
+
+std::vector<int> getRow(int rowIndex) {
+    int sz = 1;
+    std::vector<int> temp = {1};
+    std::vector<int> result(temp);
+    for (int i = 0; i < rowIndex; ++i) {
+        temp.clear();
+
+        temp.push_back(1);
+
+        for (int j = 0; j < sz - 1; ++j)
+            temp.push_back(result[j] + result[j + 1]);
+
+        temp.push_back(1);
+
+        result = temp;
+        sz++;
+    }
+    return result;        
+}
